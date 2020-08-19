8e3dfa61a162950bf30270798153e70e6e56c53c
Prabhakar Acharya <acharyaP03@gmail.com>
2020-08-19 20:16:38 +1000

    individual country select

--- src/API/api.js
+++ src/API/api.js
@@ -2,18 +2,23 @@ import axios from 'axios';
 
 const API_URL = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () =>{
+export const fetchData = async (country) =>{
+    let selectedCountryUrl = API_URL;
+
+    if(country){
+        selectedCountryUrl = `${API_URL}/countries/${country}`
+    }
     try{
         //we can straight return data like this from the response object. rather than data.confirmed and vice versa..
 
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(API_URL);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(selectedCountryUrl);
 
         //const modifiedData = { confirmed, recovered, deaths, lastUpdate }
 
         return { confirmed, recovered, deaths, lastUpdate };
 
     }catch(err){
-
+        console.log(err)
     }
 }
 
