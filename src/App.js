f53139f6ccfc5f4a425932704ff8c58fb6a4aac3
Prabhakar Acharya <acharyaP03@gmail.com>
2020-08-19 12:03:22 +1000

    basic project folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card/Cards'
+import Charts from './Components/Charts/Charts'
+import Country from './Components/Country/Country'
 
 class App extends React.Component {
   render(){
@@ -8,7 +11,11 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>App starts here </h1>
+          <Card />
+          <Charts />
+          <Country />
         </header>
+        
       </div>
     );
   }
---
+++ src/Components/Country/Country.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Country = props => {
+    return (
+        <div>
+            <h2>country</h2>
+        </div>
+    )
+}
+
+Country.propTypes = {
+
+}
+
+export default Country
